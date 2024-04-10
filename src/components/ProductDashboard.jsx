ee9cd504296b31c48c84357227ca0185d91360d3
Kanishk Pareek <knshkpareek@gmail.com>
2024-04-10 22:42:07 +0530

    changes

--- src/components/ProductDashboard.jsx
+++ src/components/ProductDashboard.jsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField,DialogContentText } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, DialogContentText } from '@mui/material';
 import Header from './Header';
 import axios from 'axios';
 import Footer from './Footer';
 import { useState } from 'react';
-
-
 
 export default function ProductDashboard() {
     const [isDialogOpen, setIsDialogOpen] = React.useState(false);
@@ -28,32 +26,41 @@ export default function ProductDashboard() {
         tagline: ''
     });
     const columns = [
-      { field: '_id', headerName: 'ID', width: 200 },
-      {
-        field: 'actions',
-        headerName: 'Actions',
-        width: 150,
-        renderCell: (params) => (
-            <Button
-                variant="contained"
-                color="secondary"
-                onClick={() => handleDeleteProduct(params.row._id)}
-            >
-                Delete
-            </Button>
-        ),
-    },
-      { field: 'title', headerName: 'Product Name', width: 400, sortable: true, valueGetter: (params) => params.row.title.shortTitle },
-      { field: 'title', headerName: 'Product Full Name', width: 300, sortable: true, valueGetter: (params) => params.row.title.longTitle },
-      { field: 'price.mrp', headerName: 'Actual Price', type: 'number', width: 290, sortable: true, valueGetter: (params) => params.row.price.mrp },
-      { field: 'price.cost', headerName: 'MRP', type: 'number', width: 250, sortable: true, valueGetter: (params) => params.row.price.cost },
-      { field: 'price.discount', headerName: 'Discount', type: 'number', width: 250, sortable: true, valueGetter: (params) => params.row.price.discount },
-      { field: 'quantity', headerName: 'Quantity ', type: 'number', width: 250, sortable: true },
-      { field: 'description', headerName: 'Description', width: 250, sortable: false },
-      { field: 'discount', headerName: 'DiscountLine', width: 250, sortable: false },
-      { field: 'tagline', headerName: 'Tagline', width: 250, sortable: false }
-  ];
+        { field: '_id', headerName: 'ID', width: 200 },
+        {
+            field: 'actions',
+            headerName: 'Actions',
+            width: 150,
+            renderCell: (params) => (
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => handleDeleteProduct(params.row._id)}
+                >
+                    Delete
+                </Button>
+            ),
+        },
+        { field: 'title', headerName: 'Product Name', width: 400, sortable: true, valueGetter: (params) => params.row.title.shortTitle },
+        { field: 'title', headerName: 'Product Full Name', width: 300, sortable: true, valueGetter: (params) => params.row.title.longTitle },
+        { field: 'price.mrp', headerName: 'Actual Price', type: 'number', width: 290, sortable: true, valueGetter: (params) => params.row.price.mrp },
+        { field: 'price.cost', headerName: 'MRP', type: 'number', width: 250, sortable: true, valueGetter: (params) => params.row.price.cost },
+        { field: 'price.discount', headerName: 'Discount', type: 'number', width: 250, sortable: true, valueGetter: (params) => params.row.price.discount },
+        { field: 'quantity', headerName: 'Quantity ', type: 'number', width: 250, sortable: true },
+        { field: 'description', headerName: 'Description', width: 250, sortable: false },
+        { field: 'discount', headerName: 'DiscountLine', width: 250, sortable: false },
+        { field: 'tagline', headerName: 'Tagline', width: 250, sortable: false }
+    ];
     const [selectedProductId, setSelectedProductId] = React.useState(null);
+    const validateProductData = (productData) => {
+        // Check if any field is empty
+        for (const key in productData) {
+            if (productData[key] === '') {
+                return false;
+            }
+        }
+        return true;
+    };
 
     const fetchData = async () => {
         try {
@@ -92,15 +99,27 @@ export default function ProductDashboard() {
         setProductData({ ...productData, [name]: value });
     };
 
-    const handleDialogSubmit = (productData) => {
+    const handleDialogSubmit = async (productData) => {
+        // Validate product data
+        if (!validateProductData(productData)) {
+            // If any field is empty, display an error message
+            alert("Please fill in all fields");
+            return;
+        }
+    
+        // If all fields are filled, proceed with submitting the data
         console.log(productData)
-        // Send data to POST API
-
+        const response = await axios.post(`https://api.flipkarttech.com/addProducts`, productData);
+        setIsDialogOpen(false);
+        setIsDeleteDialogOpen(false);
+        setInputValue(''); // Reset input value when dialog is closed
+        console.log(response)
     };
+    
 
     const handleDeleteProduct = async (productId) => {
         try {
-          console.log(`>>>>>>>>>>>>>>>..${productId}`)
+            console.log(`>>>>>>>>>>>>>>>..${productId}`)
             const response = await axios.delete(`https://api.flipkarttech.com/deleteProducts/${productId}`);
             console.log(response.data); // Log the response if needed
             // Optionally, you can fetch data again to refresh the table after deleting the product
@@ -119,7 +138,6 @@ export default function ProductDashboard() {
                 <Button style={{ marginRight: '10px', marginLeft: '10px', backgroundColor: 'lightblue', border: '1px solid lightblue', borderRadius: '4px', color: '#566161' }} onClick={() => openDialog('Add Product Number')}>
                     Add Products
                 </Button>
-        
             </div>
             <div style={{ flex: 1, marginBottom: '10px', display: 'flex', justifyContent: 'center', background: '#d8dfeb' }}>
                 {data && data.length > 0 ? (
@@ -136,11 +154,114 @@ export default function ProductDashboard() {
             <Dialog open={isDialogOpen} onClose={closeDialog}>
                 <DialogTitle style={{ backgroundColor: 'lightblue', border: '1px solid lightblue', borderRadius: '4px', color: '#566161', width: '400px', textAlign: 'center' }}>Enter Product Details</DialogTitle>
                 <DialogContent>
-                    {/* Input fields for product details */}
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="url"
+                        name="url"
+                        label="URL"
+                        fullWidth
+                        value={productData.url}
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="detailUrl"
+                        name="detailUrl"
+                        label="Detail URL"
+                        fullWidth
+                        value={productData.detailUrl}
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="shortTitle"
+                        name="shortTitle"
+                        label="Short Title"
+                        fullWidth
+                        value={productData.shortTitle}
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="longTitle"
+                        name="longTitle"
+                        label="Long Title"
+                        fullWidth
+                        value={productData.longTitle}
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="mrp"
+                        name="mrp"
+                        label="MRP"
+                        type="number"
+                        fullWidth
+                        value={productData.mrp}
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="cost"
+                        name="cost"
+                        label="Cost"
+                        type="number"
+                        fullWidth
+                        value={productData.cost}
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="discount"
+                        name="discount"
+                        label="Discount"
+                        type="number"
+                        fullWidth
+                        value={productData.discount}
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="quantity"
+                        name="quantity"
+                        label="Quantity"
+                        type="number"
+                        fullWidth
+                        value={productData.quantity}
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="description"
+                        name="description"
+                        label="Description"
+                        fullWidth
+                        value={productData.description}
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="discounts"
+                        name="discounts"
+                        label="Discounts"
+                        fullWidth
+                        value={productData.discounts}
+                        onChange={handleInputChange}
+                    />
+                    <TextField
+                        margin="dense"
+                        id="tagline"
+                        name="tagline"
+                        label="Tagline"
+                        fullWidth
+                        value={productData.tagline}
+                        onChange={handleInputChange}
+                    />
                 </DialogContent>
                 <DialogActions>
                     <Button style={{ backgroundColor: 'lightblue', border: '1px solid lightblue', borderRadius: '4px', color: '#566161', fontSize: '12px' }} onClick={closeDialog}>Cancel</Button>
-                    <Button style={{ backgroundColor: 'lightblue', border: '1px solid lightblue', borderRadius: '4px', color: '#566161', fontSize: '12px' }} onClick={handleDialogSubmit}>Submit</Button>
+                    <Button style={{ backgroundColor: 'lightblue', border: '1px solid lightblue', borderRadius: '4px', color: '#566161', fontSize: '12px' }} onClick={() => handleDialogSubmit(productData)}>Submit</Button>
                 </DialogActions>
             </Dialog>
             <Dialog open={isDeleteDialogOpen} onClose={closeDialog}>
